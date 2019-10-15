7e2c444ff89087dc1d7ebbea33a5f6cc44364629
arthur <arthur.genthial@ynov.com>
2019-10-15 17:46:58 +0200

    add morgan

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const router = require('./routes/router');
+const morgan = require('morgan');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-app.use(express.json())
-app.use(router)
+app.use(morgan());
+app.use(express.json());
+app.use(router);
 
 const port = process.env.PORT;
 app.listen(port, () => {
