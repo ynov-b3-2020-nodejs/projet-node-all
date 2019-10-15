5edede6a0461db6e5ef4265847e0499d06b0421b
MaxenceMottard <mottard.maxence@gmail.com>
2019-10-15 15:23:07 +0200

    Create AuthRouter

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,11 @@
 const express = require('expess');
 
 const UserRoutes = require('./UserRoute');
+const AuthRoutes = require('./AuthRoute');
 
 let router = express.Router();
 router = UserRoutes(router);
+router = AuthRoutes(router);
 
 module.exports = router;
 
