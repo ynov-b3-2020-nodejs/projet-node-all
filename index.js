02dcc43c82b8f40dbbb756c5bfbf3989850230c7
arthur <arthur.genthial@ynov.com>
2019-10-15 16:03:09 +0200

    change port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.json({string:'salut'});
+  res.send('salut');
 });
 app.listen(port, function () {
   console.log(`Vous êtes bien connecté sur le port: ${port}`)
