f649c7d9c045eab19c58fbf9659b870eb03e3785
Julien <julien.seixas@free.fr>
2019-10-15 14:55:22 +0200

    moise
    Splits files on 2 branch

--- _src/services/UserServices.js
+++
@@ -1,6 +0,0 @@
-const getRandomUser = ({ length }) => Math.floor(Math.random() * length);
-const getRandomElementInArray = (array) => array[getRandomIndexInArray(array)];
-
-module.exports = {
-    getRandomElementInArray,
-};
