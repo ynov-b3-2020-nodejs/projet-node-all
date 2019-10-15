f612591645d47f6527329191226723ed4d106ee4
arthur <arthur.genthial@ynov.com>
2019-10-15 15:56:43 +0200

    add port

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 projet-node-all
+
+yarn start => lancer le serveur  
+yarn dev => permet de relancer le serveur à chaque modifications   
+yarn prettier => formatte le code en fonction des règles de prettierrc
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.listen(port);
+app.get('/', (req, res) => {
+  res.json({string:'salut'});
+});
+app.listen(port, ()=> {
+  console.log('Le serveur tourne sur le port : '+port)
+});
 
