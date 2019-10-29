c652dc2adfea53f9cb62c0858bdc4aa4978c3619
Nicolas <larochenicolas.info@gmail.com>
2019-10-29 16:40:40 +0100

    Merge master

---
+++ src/routes/AuthRoute.js
@@ -0,0 +1,29 @@
+const UserService = require('../services/UserServices');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+module.exports = (router) => {
+    // #TODO connecter avec fonction du groupe B (API Custom)
+    router.post('/auth', passport.authenticate('local', {failureRedirect: '/auth'}), (req, res) => {
+        res.redirect('/');
+    });
+
+    return router;
+};
+
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+}, (email, password, done) => {
+    const user = UserService.findOneBy({email});
+    const encPassword = UserService.hashPassword(password);
+
+    if (!user || encPassword !== user.password) {
+        return done(null, false, {errors: {'email or password': 'is invalid'}});
+    }
+
+    return done(null, user);
+}));
+
+
---
+++ src/services/UserServices.js
@@ -0,0 +1,26 @@
+const MongooseService = require('./MongooseService');
+
+class UserService extends MongooseService {
+
+    constructor () {
+        super('User');
+    }
+
+    /**
+     * @param userData{ mail, lastName, firstName, password }
+     * @returns {Promise<User|boolean>}
+     */
+    async create (userData) {
+        return super.create(userData)
+    }
+
+    getRandomElementInArray(array) {
+        return array[this.getRandomIndexInArray(array)]
+    };
+
+    getRandomIndexInArray({ length }) {
+        return  Math.floor(Math.random() * length);
+    };
+
+}
+module.exports = new UserService();
