c60a0ba9c21a6e5687729551d60599bdccf66878
Julien SEIXAS <32927775+MrZyr0@users.noreply.github.com>
2019-10-29 10:13:24 +0100

    Update UserServices.js

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -4,16 +4,3 @@ const getRandomElementInArray = (array) => array[getRandomIndexInArray(array)];
 module.exports = {
     getRandomElementInArray,
 };
-
-
-// CREATE : Gianni
-
-
-// READ : Zakarya
-
-
-// UPDATE : Julien
-
-
-// DELETE : Antoine
-
